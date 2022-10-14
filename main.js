12796e1da53bbc29a2540b70c2c275ce1a2f4e2c
Daniel Lozada <danie_1987_@hotmail.com>
2022-10-14 11:28:48 -0500

    Se agrego la funcion "toggleShowInfoWhoAreUs" para mostrar u ocultar la informacion seleccionada

--- main.js
+++ main.js
@@ -5,4 +5,15 @@ menu.addEventListener("click", toggleCollapseMenu);
 
 function toggleCollapseMenu() {
     collapseMenu.classList.toggle("inactive");
-}
+    info_who_are_us.classList.add("inactive");
+};
+
+const who_are_us = document.querySelector(".who_are_us")
+const info_who_are_us = document.getElementById("info_who_are_us");
+
+who_are_us.addEventListener("click", showInfoWhoAreUs);
+
+function toggleShowInfoWhoAreUs() {
+    info_who_are_us.classList.toggle("inactive");
+    collapseMenu.classList.add("inactive");
+};
